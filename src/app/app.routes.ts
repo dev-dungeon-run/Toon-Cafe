f5b532de1d347e7a5b0054568faac1c9ee13633e
Saristeidyn <saray437933@gmail.com>
2026-03-09 20:14:44 -0600

    about u layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AboutUsComponent } from './pages/about-us/about-us/about-us.component';
+import { Login } from './pages/login/login/login';
+import { Menu } from './pages/menu/menu/menu';
+import { ShowTimes } from './pages/show-times/show-times/show-times';
+import { Home } from './pages/home/home/home';
+
+export const routes: Routes = [
+  { path: '', component: AboutUsComponent },
+  { path: 'login', component: Login },
+  { path: 'menu', component: Menu },
+  { path: 'show-times', component: ShowTimes },
+  { path: 'home', component: Home }
+];
